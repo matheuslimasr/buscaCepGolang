94f767d7294a31f29b8d2d5bd64f6529b7ec9141
Matheus Lima <matheuslima.bsantos@gmail.com>
2024-10-02 19:57:22 -0300

    criação da função BuscaCep e sua Model

---
+++ controllers/BuscaCep.go
@@ -0,0 +1,44 @@
+package controllers
+
+import (
+	"context"
+	"encoding/json"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/matheuslimasr/buscaCepGolang/Models"
+)
+
+func HandlerBuscaCep(w http.ResponseWriter, r *http.Request) {
+	cep := r.URL.Query().Get("cep")
+	ctx, cancel := context.WithTimeout(r.Context(), time.Second*30)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", "https://viacep.com.br/ws/"+cep+"/json/", nil)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	response, err := http.DefaultClient.Do(req)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+
+	var ViaCep Models.ViaCep
+
+	err = json.Unmarshal(body, &ViaCep)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	// nexts codes
+}
--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/matheuslimasr/buscaCepGolang/controllers"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", controllers.HandlerBuscaCep)
+
+	fmt.Println("Servidor Iniciado!")
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		panic(err)
+	}
 }
